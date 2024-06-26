5ec2679d7bf926b968d1a49a3d9e1670168dfcaa
Muhammad Ravat <m0ravat736@gmail.com>
2024-06-26 22:29:03 +0100

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,76 +9,7 @@ public class Main {
     static Module operate = new Module("OperatingSystems", 7);
     static Module robots = new Module("Robotics", 6);
     static Module current = serverWeb;
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        boolean loop = false;
-        while (!loop){
-            System.out.println("Please select which class you want to enrol in: " +
-                    "1 - Server side web development " +
-                    "2 - Client side web development" +
-                    "3 - Mobile App Development" +
-                    "4 - Operating Systems" +
-                    "5 - Robotic Principles" +
-                    "0 - Quit Application");
-            try {
-                int menu = input.nextInt();
-                switch (menu) {
-                    case 1 -> current=serverWeb;
-                    case 2 -> current=clientWeb;
-                    case 3 -> current=mobile;
-                    case 4 -> current=operate;
-                    case 5 -> current=robots;
-                    case 0 -> {
-                        loop=true;
-                        System.out.println("Thank you for using the Plane Management application");
-                    }
-                    default -> System.out.println("Please enter a valid menu option");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Enter an integer please");
-                input.nextLine();
-            }
-            int x = 1;
-            while (x==1){
-                System.out.println("Would you like to: " +
-                        "A - Book a place" +
-                        "B - Cancel a place" +
-                        "C - Find the first available place" +
-                        "D - Find how many openings are left" +
-                        "E - Find the info for a seat" +
-                        "F - Return to the original menu" +
-                        "G - Quit Application");
-                String choice = input.next();
-                int row = 0;
-                int col = 0;
-                switch (choice) {
-                    case "A":
-                        row = getRow();
-                        col = getCol();
-                        current.buySeat(row,col);
-                    case "B":
-                        row = getRow();
-                        col = getCol();
-                        current.cancelSeat(row,col);
-                    case "C":
-                        current.findFirstSeat();
-                    case "D":
-                        current.seatsLeft();
-                    case "E":
-                        row = getRow();
-                        col = getCol();
-                        current.seatInfo(row,col);
-                    case "F":
-                        x=2;
-                    case "G":
-                        x=2;
-                        loop=true;
-                        break;
-                }
-            }
-        }
-    }
+    public static int menu = 1;
 
     private static int getCol(){
         Scanner input = new Scanner(System.in);
@@ -86,10 +17,10 @@ public class Main {
         int maxcol = current.getSize();
         boolean colValid = false;
         while (!colValid){
-            System.out.println("Enter a column number between 0 and " + maxcol);
+            System.out.println("Enter a column number between 0 and " + (maxcol-1));
             col = input.nextInt();
             if (col > -1){
-                if (col < (maxcol+1)){
+                if (col < maxcol){
                     colValid = true;
                 }
             }
@@ -102,10 +33,10 @@ public class Main {
         int maxrow = 4;
         boolean rowValid = false;
         while (!rowValid){
-            System.out.println("Enter a column number between 0 and " + maxrow);
+            System.out.println("Enter a row number between 0 and " + 3);
             row = input.nextInt();
             if (row > -1){
-                if (row < (maxrow+1)){
+                if (row < (maxrow)){
                     rowValid = true;
                 }
             }
@@ -113,4 +44,109 @@ public class Main {
         return row;
     }
 
+
+    public static void menu1(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please select which class you want to enrol in\n " +
+                "1 - Server side web development\n " +
+                "2 - Client side web development\n" +
+                "3 - Mobile App Development\n" +
+                "4 - Operating Systems\n" +
+                "5 - Robotic Principles\n" +
+                "0 - Quit Application\n");
+        try {
+            int choice = input.nextInt();
+            switch (choice) {
+                case 1 -> {
+                    current = serverWeb;
+                    menu = 2;
+                }
+                case 2 -> {
+                    current=clientWeb;
+                    menu = 2;
+                }
+                case 3 -> {
+                    current = mobile;
+                    menu = 2;
+                }
+                case 4 -> {
+                    current = operate;
+                    menu = 2;
+                }
+                case 5 -> {
+                    current = robots;
+                    menu = 2;
+                }
+                case 0 -> {
+                    menu=3;
+                    System.out.println("Thank you for using the Plane Management application");
+                }
+                default -> System.out.println("Please enter a valid menu option");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Enter an integer please");
+            input.nextLine();
+        }
+    }
+    public static void menu2(){
+        current.getSeats();
+        System.out.println("\n");
+        Scanner input = new Scanner(System.in);
+        int x;
+        System.out.println("Would you like to: \n " +
+                "A - Book a place\n" +
+                "B - Cancel a place\n" +
+                "C - Find the first available place\n" +
+                "D - Find how many openings are left\n" +
+                "E - Find the info for a seat\n" +
+                "F - Return to the original menu\n" +
+                "G - Quit Application");
+        String choice = input.next();
+        int row = 0;
+        int col = 0;
+        switch (choice) {
+            case "A":
+                row = getRow();
+                col = getCol();
+                current.buySeat(row,col);
+                break;
+            case "B":
+                row = getRow();
+                col = getCol();
+                current.cancelSeat(row,col);
+                break;
+            case "C":
+                current.findFirstSeat();
+                break;
+            case "D":
+                current.seatsLeft();
+                break;
+            case "E":
+                row = getRow();
+                col = getCol();
+                current.seatInfo(row,col);
+                break;
+            case "F":
+                menu=1;
+                break;
+            case "G":
+                menu=3;
+                break;
+        }
+    }
+    public static void main(String[] args) {
+        boolean loop = true;
+        while (loop == true) {
+            if (menu == 1) {
+                menu1();
+            } else if (menu == 2) {
+                menu2();
+            } else {
+                loop = false;
+            }
+        }
+    }
+
 }
+
+
